6da48cb72c2e05467b7068dd1574d49da0761427
EL <elvin.dwi@ist.id>
2022-04-13 22:27:43 +0700

    creat first app

---
+++ app/src/main/java/com/example/learn_android/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.learn_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Inisialisasi komponen nya
+        val edTanggalLahir = findViewById<EditText>(R.id.ed_tanggal_lahir)
+        val btnKlik = findViewById<Button>(R.id.btn_klik)
+        val tvHasil = findViewById<TextView>(R.id.tv_hasil)
+
+        // logic
+        btnKlik.setOnClickListener {
+            val input = edTanggalLahir.text.toString()
+            val hasil = when(input.toInt()){
+                in 1946..1964 -> "Baby Boomers"
+                in 1965..1980 -> "X"
+                in 1981..2025 -> "Generasi Koplo"
+                else -> "Tidak diakui"
+             }
+
+            tvHasil.text = "Kami generasi $hasil"
+        }
+    }
+}
