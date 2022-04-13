95dc5d6f2ecf14c96c58b9acb9757aa42418491b
EL <elvin.dwi@ist.id>
2022-04-13 23:01:52 +0700

    add new activity and move act function

---
+++ app/src/main/java/com/example/learn_android/MenuActivity.kt
@@ -0,0 +1,17 @@
+package com.example.learn_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    fun moveToPenentuGenerasi(view: View) {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
